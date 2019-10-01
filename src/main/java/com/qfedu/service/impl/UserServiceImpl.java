a7eda1fab3564ac8b18fc3813fe62caef0fb6629
BECAUSEOFYOUI <55569095+BECAUSEOFYOUI@users.noreply.github.com>
2019-10-01 11:52:52 +0800

    Update UserServiceImpl.java
    接口实现更改

--- src/main/java/com/qfedu/service/impl/UserServiceImpl.java
+++ src/main/java/com/qfedu/service/impl/UserServiceImpl.java
@@ -2,5 +2,5 @@ package com.qfedu.service.impl;
 
 import com.qfedu.service.UserService;
 
-public class UserServiceImpl extends UserService {
+public class UserServiceImpl implements UserService {
 }
